6f03c760d37c743e76b379923cb83e14826ea3d6
KanataN <1725576689@qq.com>
2026-04-07 22:22:22 +0800

    添加ls,version命令 添加了alias以及路径校验 将if改为switch

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <shellapi.h>
 
 #pragma comment(lib, "Shell32.lib")
+
+#define FA_VERSION "0.0.2"
+
 std::string getExeDir() {
     char path[MAX_PATH];
     GetModuleFileNameA(nullptr,path,MAX_PATH);
@@ -21,7 +24,7 @@ void loadMappings(const std::string& filename,std::unordered_map<std::string,std
         if (createFile.is_open()) {
             createFile.close();
         }else {
-            std::cout << "文件读取错误" << filename << std::endl;
+            std::cout << "file read error" << filename << std::endl;
         }
     }
 
@@ -43,6 +46,33 @@ void saveMappings(const std::string& filename,std::unordered_map<std::string,std
     file.close();
 }
 
+void listMappings(const std::unordered_map<std::string,std::string>& map) {
+    if (map.empty()) {
+        std::cout << "No mappings found." << std::endl;
+        return;
+    }
+
+    std::cout << "Alias mappings:" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    for (const auto& pair : map) {
+        std::cout << pair.first << " -> " << pair.second << std::endl;
+    }
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "Total: " << map.size() << " mapping(s)" << std::endl;
+}
+
+enum class Command {
+    VERSION,
+    LS,
+    OPEN
+};
+
+Command parseCommand(const std::string& cmd) {
+    if (cmd == "version") return Command::VERSION;
+    if (cmd == "ls") return Command::LS;
+    return Command::OPEN;
+}
+
 int main(int argc,char* argv[]) {
     if (argc < 2) {
         std::cout << "Enter 'fa help' to get commands used." <<std::endl;
@@ -55,21 +85,35 @@ int main(int argc,char* argv[]) {
     std::string arg1 = argv[1];
 
     if (argc == 2) {
-        //打开资源管理器
-        auto it = mappings.find(arg1);
-        if (it != mappings.end()) {
-            ShellExecuteA(
-                nullptr,
-                "explore",
-                it->second.c_str(),
-                nullptr,
-                nullptr,
-                SW_SHOWNORMAL
-                );
-            std::cout << "success" <<std::endl;
+        switch (parseCommand(arg1)) {
+            case Command::VERSION:
+                std::cout << "Folder-Alias v" << FA_VERSION << std::endl;
+                break;
+            case Command::LS:
+                listMappings(mappings);
+                break;
+            case Command::OPEN: {
+                auto it = mappings.find(arg1);
+                if (it != mappings.end()) {
+                    HINSTANCE hRet = ShellExecuteA(
+                        nullptr,
+                        "explore",
+                        it->second.c_str(),
+                        nullptr,
+                        nullptr,
+                        SW_SHOWNORMAL
+                        );
+                    if ((INT_PTR)hRet <= 32) {
+                        std::cerr << "ShellExecuteA failed with code: " << (int)(INT_PTR)hRet << std::endl;
+                    }else {
+                        std::cout << "success" << std::endl;
+                    }
+                }else {
+                    std::cout << "Cannot find object" << std::endl;
+                }
+                break;
+            }
         }
-
-        return 0;
     }
 
     if (argc == 3) {
